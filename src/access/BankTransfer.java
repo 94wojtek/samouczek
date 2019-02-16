c4bd482ed982306d19557b83e79d8b5aa66c6504
94wojtek <45543887+94wojtek@users.noreply.github.com>
2019-02-16 17:55:07 +0000

    Update BankTransfer.java

--- src/access/BankTransfer.java
+++ src/access/BankTransfer.java
@@ -1,5 +1,21 @@
 package access;
 
 public interface BankTransfer {
-    void transfer(BankAccount from, BankAccount to, double amount);
+    //void transfer(BankAccount from, BankAccount to, double amount);
+    
+    static void transfer(BankAccount from, BankAccount to, double amount) {
+
+        double amountWithFee = amount + 1;
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be larger than 0.");
+        }
+
+        if (amountWithFee > 0 && amountWithFee <= from.getBalance()) {
+            from.withdraw(amountWithFee);
+            to.deposit(amount);
+        } else {
+            throw new IllegalArgumentException("Amount can not be larger than available funds. Available funds: " + from.getBalance());
+        }
+    }
 }
